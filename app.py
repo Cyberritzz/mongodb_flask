671de5ce2b8b61cbf3aabbbb256446c44624cdad
Haris Sofyan <comnex638@gmail.com>
2023-06-28 22:57:12 +0700

    hampir selesai

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from pymongo import MongoClient
 from bson import ObjectId
 from jsonschema import validate, ValidationError
 from flask_json_schema import JsonSchema , JsonValidationError
+from pymongo.errors import BulkWriteError
 
 app = Flask(__name__)
 app.secret_key = 'harissofyan'
@@ -50,7 +51,24 @@ def data(user_id=None):
         return redirect('/')
 
     data_input = list(collection.find())
-    return render_template('data.html', username_pengguna=username_pengguna, data_input=data_input)
+    total_count = collection.count_documents({})
+
+    pipeline = [
+        {
+            '$group': {
+                '_id': None,
+                'total': {'$sum': '$nominal'}
+            }
+        }
+    ]
+
+    result = list(collection.aggregate(pipeline))
+
+    total_sum = 0
+    if result:
+        total_sum = result[0]['total']
+
+    return render_template('data.html', username_pengguna=username_pengguna, data_input=data_input, int=int, total_count=total_count, total_sum=total_sum)
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -64,7 +82,7 @@ def login():
             session['user_id'] = str(user['_id'])
             return redirect('/data')
         else:
-            return jsonify({'message': 'Login failed'})
+            return redirect('/')
     else:
         return render_template('index.html')
 
@@ -101,6 +119,11 @@ def add_data():
         nomor_hp = data.get('nomor_hp')
         nominal = int(data.get('nominal'))  # Konversi ke float
 
+        if transaksi == 'Pemasukan':
+            nominal = abs(nominal)  # Mengubah nilai nominal menjadi positif
+        elif transaksi == 'Pengeluaran':
+            nominal = -abs(nominal)  # Mengubah nilai nominal menjadi negatif
+
         data = {
             'tanggal': tanggal,
             'transaksi': transaksi,
@@ -121,7 +144,7 @@ def add_data():
                     'ukm': {'type': 'string'},
                     'email': {'type': 'string', 'format': 'email'},
                     'nomor_hp': {'type': 'string'},
-                    'nominal': {'type': 'number', 'minimum': 0}
+                    'nominal': {'type': 'number'}
                 },
                 'required': ['tanggal', 'transaksi', 'nama', 'ukm', 'email', 'nomor_hp', 'nominal']
             })
@@ -133,7 +156,83 @@ def add_data():
         return redirect('/data')
 
     return render_template('tambah_data.html')
+@app.route('/hapus/<data_id>', methods=['POST'])
+def hapus_data(data_id):
+    data = collection.find_one({'_id': ObjectId(data_id)})
+    if data is not None:
+        collection.delete_one({'_id': ObjectId(data_id)})
+        return redirect('/data')
+    else:
+        return render_template('data.html', data_id=data_id)
+
+@app.route('/update/<data_id>', methods=['POST', 'GET'])
+def update_data(data_id):
+    perbaiki_data = collection.find_one({'_id': ObjectId(data_id)})
+    if perbaiki_data is None:
+        return render_template('data.html', data_id=data_id)
+
+    if request.method == 'POST':
+        updated_data = request.form
+        new_name = updated_data.get('updateName')
+        new_ukm = updated_data.get('updateUkm')
+        new_jenis = updated_data.get('updateJenis')
+        new_tanggal = updated_data.get('updateTanggal')
+        new_nominal = int(updated_data.get('updateNominal'))
+        if new_jenis == 'Pemasukan':
+            new_nominal = abs(new_nominal)  # Mengubah nilai nominal menjadi positif
+        elif new_jenis == 'Pengeluaran':
+            new_nominal = -abs(new_nominal)  # Mengubah nilai nominal menjadi negatif
+
+        collection.update_one(
+            {'_id': ObjectId(data_id)},
+            {'$set': {
+                'nama': new_name,
+                'ukm': new_ukm,
+                'transaksi': new_jenis,
+                'tanggal': new_tanggal,
+                'nominal': new_nominal
+            }}
+        )
+
+        return redirect('/data')
+
+    return render_template('data.html', perbaiki_data=perbaiki_data)
+@app.route('/data_banyak')
+def tambah_banyak():
+    return render_template('tambah_banyak.html')
+
+@app.route('/proses', methods=['POST'])
+def proses():
+    nama = request.form.getlist('nama[]')
+    ukm = request.form.getlist('ukm[]')
+    jenis = request.form.getlist('jenis[]')
+    tanggal = request.form.getlist('tanggal[]')
+    nominal = request.form.getlist('nominal[]')
+
+    bulk_operations = []
+    for i in range(len(nama)):
+        if jenis[i] == 'Pemasukan':
+            nominal[i] = abs(int(nominal[i]))  # Mengubah nilai nominal menjadi positif
+        elif jenis[i] == 'Pengeluaran':
+            nominal[i] = -abs(int(nominal[i]))  # Mengubah nilai nominal menjadi negatif
+
+        data = {
+            'nama': nama[i],
+            'ukm': ukm[i],
+            'transaksi': jenis[i],
+            'tanggal': tanggal[i],
+            'nominal': nominal[i]
+        }
+        bulk_operations.append(data)
+
+    try:
+        result = collection.insert_many(bulk_operations)
+        return redirect('/data')
+    except BulkWriteError as e:
+        return "Terjadi kesalahan saat menyimpan data"
+
 
 if __name__ == '__main__':
     check_database_connection()
     app.run(debug=True)
+
